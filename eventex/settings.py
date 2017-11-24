6a752650e33fc43f18108eea1f40ab0d76a40786
rfdeoliveira <rfdeoliveira.pmsp@gmail.com>
2017-11-24 15:10:47 -0200

    Modified settings.py

--- eventex/settings.py
+++ eventex/settings.py
@@ -99,8 +99,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # E-mail Configuration
 EMAIL_BACKEND = config('EMAIL_BACKEND')
