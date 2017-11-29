8c51807bd3094c89c3e5b0cbcd3e407a366288dc
rfdeoliveira <rfdeoliveira.pmsp@gmail.com>
2017-11-28 23:02:59 -0200

    Persisting Subscription

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Ronaldo Oliveira',
+            cpf='12345678901',
+            email='ronaldo@oliveira.net',
+            phone='11-2456-7894'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at attr"""
+        self.assertIsInstance(self.obj.created_at, datetime)
--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,6 +1,7 @@
 from django.core import mail
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 class SubscribeGet(TestCase):
@@ -65,6 +66,9 @@ class SubscribePostValid(TestCase):
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
 
+    def test_save_subscription(self):
+        self.assertTrue(Subscription.objects.exists())
+
 
 class SubscribePostInvalid(TestCase):
     def setUp(self):
@@ -84,6 +88,9 @@ class SubscribePostInvalid(TestCase):
     def test_form_has_errors(self):
         form = self.resp.context['form']
         self.assertTrue(form.errors)
+
+    def test_dont_save_subscription(self):
+        self.assertFalse(Subscription.objects.exists())
 
 
 class SubscribeSuccessMessage(TestCase):
