4b7d78a8986e0c69a7d91b5bdb7e0ba8208aea57
rfdeoliveira <rfdeoliveira.pmsp@gmail.com>
2018-03-08 03:08:36 -0300

    Talk

--- eventex/core/tests/test_view_talk_list.py
+++ eventex/core/tests/test_view_talk_list.py
@@ -1,9 +1,22 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
+from eventex.core.models import Talk, Speaker
 
 
 class TalkListGet(TestCase):
     def setUp(self):
+        t1 = Talk.objects.create(title='Título da Palestra', start='10:00',
+                                 description='Descrição da palestra.')
+        t2 = Talk.objects.create(title='Título da Palestra', start='13:00',
+                                 description='Descrição da palestra.')
+
+        speaker = Speaker.objects.create(name='Ronaldo Oliveira',
+                                         slug='ronaldo-oliveira',
+                                         website='http://ronaldo-oliveira.net')
+
+        t1.speakers.add(speaker)
+        t2.speakers.add(speaker)
+
         self.resp = self.client.get(r('talk_list'))
 
     def test_get(self):
@@ -19,9 +32,23 @@ class TalkListGet(TestCase):
             (1, '13:00'),
             (2, '/palestrantes/ronaldo-oliveira/'),
             (2, 'Ronaldo Oliveira'),
-           (2, 'Descrição da palestra'               ),
+            (2, 'Descrição da palestra.'),
         ]
 
         for count, expected in contents:
             with self.subTest():
                 self.assertContains(self.resp, expected, count)
+
+    def test_context(self):
+        variables = ['morning_talks', 'afternoon_talks']
+
+        for key in variables:
+            with self.subTest():
+                self.assertIn(key, self.resp.context)
+
+
+class TalkListGetEmpty(TestCase):
+    def test_get_empty(self):
+        response = self.client.get(r('talk_list'))
+        self.assertContains(response, 'Ainda não existem palestras da manhã.')
+        self.assertContains(response, 'Ainda não existem palestras da tarde.')
